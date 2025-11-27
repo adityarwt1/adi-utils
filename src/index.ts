ebfab4c7dc6685227c7ef2ebbd458c30792b12a7
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-27 22:05:18 +0530

    truncate added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './funtions'
 export * from "./types"
 export * from "./images/Convertintobase64"
-export * from "./string/slugify"
+export * from "./string/slugify/Slugify-v1"
+export * from "./string/Truncate/truncate-v1"
---
+++ src/string/Truncate/truncate-v1.ts
@@ -0,0 +1,5 @@
+export function truncate(text:string , length:number){
+    return text.length > length ? text.slice(0, length ? length :3 ):text
+}
+///truncate("Aditya is learning JavaScript", 10) => Aditya is...
+
