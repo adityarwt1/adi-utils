13838211276ecdda25ceb404783f5c6b3580fc9f
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-27 22:06:13 +0530

    old thing managed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export * from './funtions'
-export * from "./types"
 export * from "./images/Convertintobase64"
 export * from "./string/slugify/Slugify-v1"
 export * from "./string/Truncate/truncate-v1"
